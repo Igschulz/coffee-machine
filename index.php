eeb60a3822bda5cffbd165a3f0454220f30b43af
Igschulz <ischulz@mail.ru>
2020-09-07 12:43:20 +0300

    Внесли изменения в style

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Кофе-машина</title>
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-6 coffee-list">
+          <div class="coffee-item">
+            <img src="img/americano.png" alt="Американо">
+            <span>Американо - 50руб</span>
+          </div>
+          <div class="coffee-item">
+            <img src="img/cappuccino.png" alt="Капучино">
+            <span>Капучино - 73руб</span>>
+          </div>
+          <div class="coffee-item">
+            <img src="img/espresso.png" alt="Эспрессо">
+            <span>Эспрессо - 61руб</span>>
+          </div>
+          <div class="coffee-item">
+            <img src="img/latte.jpg" alt="Латтэ">
+            <span>Латтэ - 112руб</span>>
+          </div>
+          <div class="col-6 coffee-oper">
+          </div>
+        </div>
+      </div>
+    </div>
+   
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
